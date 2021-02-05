999de9ad532d0acf5b91ff98dcc54d9690ebbf50
Jesus Reyes <jesusareyes01@gmail.com>
2021-02-04 22:18:51 -0600

    added react-router-dom and replaced a tag with Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
 
 // this.setState({
 //   subject: 'Hello! This is a new subject'
@@ -11,26 +13,17 @@ import ListContacts from './ListContacts';
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: "karen",
-        name: "Karen Isgrigg",
-        handle: "@karen_isgrigg",
-        avatarURL: "http://localhost:5001/karen.jpg"
-      },
-      {
-        id: "richard",
-        name: "Richard Kalehoff",
-        handle: "@richardkalehoff",
-        avatarURL: "http://localhost:5001/richard.jpg"
-      },
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        handle: "@tylermcginnis",
-        avatarURL: "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: [],
+    screen: "list",
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
   }
   
   removeContact = (contact) => {
@@ -44,16 +37,23 @@ class App extends Component {
           return c.id !== contact.id
         })
     }))
+    ContactsAPI.remove(contact)
   }
   //
 
   render() {
     return (
       <div>
+        {this.state.screen === 'list' && (
         <ListContacts 
           contacts={this.state.contacts}
           onDeleteContact={this.removeContact}
-        />
+          onNavigate={()=> {
+            this.setState({screen: 'create'})
+          }}
+        />)}
+        {this.state.screen === 'create' && (
+        <CreateContact />)}
       </div>
     );
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ListContacts extends Component {
     static propTypes = {
@@ -23,7 +24,7 @@ class ListContacts extends Component {
     
     render() {
         const { query } = this.state;
-        const {contacts, onDeleteContact} = this.props;
+        const {contacts, onDeleteContact, onNavigate} = this.props;
 
         const showingContacts = query === ""
             ? contacts
@@ -40,8 +41,13 @@ class ListContacts extends Component {
                         value = {query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
+                    <Link
+                        to='/create'
+                        className ='add-contact'
+                        >Add Contact
+                        </Link>
                 </div>
-                {/* {query !== '' && ( */}
+                {/* {this.state.query !== '' && ( */}
                 {showingContacts.length !== contacts.length && (
                     <div className = 'showing-contacts'>
                         <span>Now showing {showingContacts.length} of {contacts.length} </span>
@@ -86,7 +92,7 @@ class ListContacts extends Component {
         )
     }
 }
-//Functional Component
+//Functional Component prop-type setup
 // ListContacts.propTypes = {
 //     contacts: PropTypes.array.isRequired,
 //     onDeleteContact: PropTypes.func.isRequired
